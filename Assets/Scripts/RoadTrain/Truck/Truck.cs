7b602dedeea571128d7e7760ba40a79e835f8d4a
Ша Гри <Grissshhha@outlook.com>
2025-11-05 21:31:43 +0500

    add class trusk, installer boost of trusk

--- Assets/Scripts/RoadTrain/Truck/Truck.cs
+++ Assets/Scripts/RoadTrain/Truck/Truck.cs
@@ -4,31 +4,42 @@ using YG;
 
 namespace RoadTrane
 {
+    [RequireComponent(typeof (SpriteRenderer))]
     public class Truck : MonoBehaviour
     {
         public const string SpeedTrusk = "Speeder";
         public const string Attacker = "Attacker";
         public const string Stealther = "Stealther";
 
-        public string TypeTrusk { get; private set; }
+        private SpriteRenderer _spriteRenderer;
 
-        //   public string TypeTrusk
+        [SerializeField] private Sprite _speedSprite;
+        [SerializeField] private Sprite _attackerSprite;
+        [SerializeField] private Sprite _stealtherSprite;
+        public string TypeTrusk { get; private set; }
 
         private void OnEnable()
         {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+
             string saved = YG2.saves.TruskType;
 
             if (saved == SpeedTrusk)
             {
                 TypeTrusk = SpeedTrusk;
+                _spriteRenderer.sprite = _speedSprite;
             }
             else if (saved == Attacker)
             {
                 TypeTrusk = Attacker;
+                _spriteRenderer.sprite = _attackerSprite;
+
             }
             else if (saved == Stealther)
             {
                 TypeTrusk = Stealther;
+                _spriteRenderer.sprite = _stealtherSprite;
+
             }
         }
     }
